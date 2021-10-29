ec8981d72315184841291b589603f700fc614c63
dipdastoko <dipdastoko@gmail.com>
2021-10-30 01:09:32 +0600

    promt

--- browser-api/alert.js
+++ browser-api/alert.js
@@ -5,3 +5,25 @@ console.log('gello');
 const maComming = () => {
     alert('Ma is coming');
 }
+
+const askPicnic = () => {
+    const response = confirm('are you going to picnic');
+    console.log(response);
+
+    if (response === true) {
+        alert('taka dao')
+    }
+    else {
+        alert('dore gia mor');
+    }
+}
+
+const askName = () => {
+    const name = prompt('what is your name');
+    if (name) {
+        console.log(name);
+    }
+    else {
+        console.log('mara kha');
+    }
+}
