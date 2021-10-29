4f315a5257978b372ee1388d14a41b0519478221
dipdastoko <dipdastoko@gmail.com>
2021-10-30 00:54:38 +0600

    alert

---
+++ browser-api/alert.js
@@ -0,0 +1,7 @@
+console.log('gello');
+
+// alert('Ma is coming');
+
+const maComming = () => {
+    alert('Ma is coming');
+}
---
+++ browser-api/explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {
+    name: 'Jolil', friend: 'borsha', job: 'business',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+}
+student.makeMovie('Din the day');
+// document.getElementById('name');
