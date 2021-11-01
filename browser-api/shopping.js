0b1f940c8f5a1720c78c8f7424c8c08d1e6b41e7
dipdastoko <dipdastoko@gmail.com>
2021-11-01 11:12:12 +0600

    local storge

---
+++ browser-api/shopping.js
@@ -0,0 +1,21 @@
+const addItem = () => {
+    const nameField = document.getElementById('product-name');
+    const name = nameField.value;
+    //display in the ui
+    displayProduct(name);
+    //add to local storege
+
+
+
+
+
+    nameField.value = '';
+}
+
+const displayProduct = name => {
+    const ul = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerText = name;
+    ul.appendChild(li);
+
+}
