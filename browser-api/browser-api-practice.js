914513322d610338bfeb7712457a5ad1d277b729
dipdastoko <dipdastoko@gmail.com>
2021-11-01 15:39:04 +0600

    practice

--- browser-api/browser-api-practice.js
+++ browser-api/browser-api-practice.js
@@ -1,10 +1,16 @@
-//1.
-setTimeout(function () {
-    console.log('setTimeOut after 3.5s');
-}, 3500);
+// //1.
+// setTimeout(function () {
+//     console.log('setTimeOut after 3.5s');
+// }, 3500);
 
-//2.
-const promptMessage = prompt();
+// //2.
+// const promptMessage = prompt();
 
-const alertMessage = parseInt(promptMessage) + 200;
-alert(alertMessage.toString());
+// const alertMessage = parseInt(promptMessage) + 200;
+// alert(alertMessage.toString());
+
+//3.
+const response = confirm('Do you want to see your website location?');
+if (response === true) {
+    console.log(location.href);
+}
